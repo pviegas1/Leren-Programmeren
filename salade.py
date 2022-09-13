45b0e300377ed09cb03e9132de309b1779e4061b
pviegas1 <113175927+pviegas1@users.noreply.github.com>
2022-09-13 14:08:47 +0200

    eerste commit

---
+++ salade.py
@@ -0,0 +1,50 @@
+print ('berkenen van de ingredienten')
+aantalkomkommers =float( input ('voor hoeveek komkommers?') )
+mespuntjesperkomkommer = 1 
+versedillen = 15 * aantalkomkommers
+tenenknoflook = 2 * aantalkomkommers
+
+griekseyogurt = 500 * aantalkomkommers
+wittewijnazijn = 1 * aantalkomkommers
+eetlepelextraviergeolijfolie = 3 * aantalkomkommers
+
+
+print (versedillen, 'gram verse dillen')
+print (griekseyogurt, 'ml griekse yogurt')
+print(wittewijnazijn, 'lepel wittewijnazijn')
+print(eetlepelextraviergeolijfolie, 'eetlepel extra vierge olijfolie')
+
+
+
+
+
+print(tenenknoflook, "aantal tenen knoflook")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
