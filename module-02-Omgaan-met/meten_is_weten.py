be1b7249d905e2f05d641c2b7ab905de12d3e075
pviegas1 <113175927+pviegas1@users.noreply.github.com>
2022-12-06 14:39:37 +0100

    elif statement

--- module-02-Omgaan-met/meten_is_weten.py
+++ module-02-Omgaan-met/meten_is_weten.py
@@ -1,5 +1,7 @@
-a=3
-b=2
-
-if a > b :print('a is het grootste getal')
+A = 3 
+B = 2 
+3 (max)
+2 (min)
+if A > B:print ('a is het grootste getal')
+elif B < A: print ("b is het kleinste getal"(3))
 
